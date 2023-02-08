43107ba27a5801ce5bbbb350f99e0d1cd63e0821
goaltvapp <76167290+goaltvapp@users.noreply.github.com>
2023-02-08 08:48:38 +0630

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(".siderbar_menu li").click(function(){
+  $(".siderbar_menu li").removeClass("active");
+  $(this).addClass("active");
+});
+
+$(".hamburger").click(function(){
+  $(".wrapper").addClass("active");
+});
+
+$(".close, .bg_shadow").click(function(){
+  $(".wrapper").removeClass("active");
+});
